6fc1778705389bd6cbc6f7ac1417cc01a010d30c
jmwoo <jim@jane.com>
2017-03-05 16:35:39 -0700

    add db project

---
+++ database/dbo/Tables/Notes.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Notes] (
+    [Id]          INT              IDENTITY (1, 1) NOT NULL,
+    [Uid]         UNIQUEIDENTIFIER DEFAULT (newid()) NULL,
+    [DateCreated] DATETIME2 (7)    CONSTRAINT [DF_DateCreated] DEFAULT (getutcdate()) NOT NULL,
+    [Latitude]    DECIMAL (9, 6)   NOT NULL,
+    [Longitude]   DECIMAL (9, 6)   NOT NULL,
+    [Radius]      INT              NOT NULL,
+    [Body]        NVARCHAR (MAX)   NULL,
+    [Score]       INT              DEFAULT ((0)) NOT NULL,
+    [Subject]     VARCHAR (MAX)    DEFAULT (NULL) NULL,
+    [UserId]      INT              DEFAULT ((1)) NOT NULL,
+    [IsAnonymous] BIT              DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [Ct_ID] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ database/dbo/Tables/Votes.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Votes] (
+    [Id]          INT              IDENTITY (1, 1) NOT NULL,
+    [Uid]         UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [NoteId]      INT              NOT NULL,
+    [UserId]      INT              NOT NULL,
+    [DateCreated] DATETIME2 (7)    DEFAULT (getutcdate()) NOT NULL,
+    [Value]       INT              NOT NULL,
+    CONSTRAINT [PK_Votes] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [UQ_vote] UNIQUE NONCLUSTERED ([NoteId] ASC, [UserId] ASC)
+);
+
